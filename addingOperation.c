116fab70e7c7e118ef1885dee62020079dc324e8
Parthip <parthip.mp303@gmail.com>
2025-08-18 22:27:33 +0530

    Added Addition operation calculator

---
+++ addingOperation.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    double number1;
+    double number2;
+
+    printf("Enter the first number");
+    scanf("%lf", &number1);
+    printf("Enter the second number");
+    scanf("%lf", &number2);
+
+    printf("Answer: %f", number1 + number2);
+    
+
+}
