5afec64fc24992af03570c9f4c30f4413afa7d93
Parthip <parthip.mp303@gmail.com>
2025-08-20 23:08:26 +0530

    Updated Mad libs game

--- madLibsGame.c
+++ madLibsGame.c
@@ -4,17 +4,19 @@ int main()
 {
     char colour[20];
     char pluralNoun[20];
-    char celebrity[20];
+    char celebrityA[20];
+    char celebrityB[20];
 
     printf("Enter a colour");
     scanf("%s", colour);
     printf("Enter a plural noun");
     scanf("%s", pluralNoun);
-    printf("Enter the celebrity");
-    scanf("%s", celebrity);
+    printf("Enter the celebrity ");
+    scanf("%s%s", celebrityA ,celebrityB);
+//%s%s is to denote two sets of stringe, or we could use fgets which is more logical, just tried a diff thing
 
     printf("Roses are %s\n", colour);
     printf("%s are green\n", pluralNoun);
-    printf("I love %s\n", celebrity);
+    printf("I love %s %s\n", celebrityA, celebrityB);
 
 }
