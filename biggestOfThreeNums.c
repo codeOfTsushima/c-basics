616b2eb1b3680d76abea9dcd98f61caaaf37330a
Parthip <parthip.mp303@gmail.com>
2025-08-22 22:07:43 +0530

    Added Biggest of 3 numbers

--- biggestOfThreeNums.c
+++ biggestOfThreeNums.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 int max(int num1, int num2, int num3){
     int result;
-    if (num1>=num2 && num1 >= num3){//%%Is a logical operator and it's function is to add another condition in here
+    if (num1>=num2 && num1 >= num3){// %%Is a logical operator and it's function is to add another condition in here
         result = num1;
-    }    else {
-            result = num2;
-        }
+    }    else if (num2 >= num1 && num2 >= num3){
+        result = num2;
+    } else {
+            result = num3;
+}
     return result;
-    }
+}
 
-int main()
-{
-    printf("%d", max(4,10));
+
+int main(){
+    printf("%d", max(4,10, 40));
+    return 0;
 }
