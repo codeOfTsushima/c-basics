55c70b2bc57493e96c9d82a2580f9f195c58a5db
Parthip <parthip.mp303@gmail.com>
2025-08-25 20:53:21 +0530

    Added about for loops

---
+++ forLoops.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int luckyNUmber[] = {4,7,10,3,2,47,77};
+    
+    int i;
+    for ( i = 1; i <=6; i++)
+    {
+        printf("%d\n",luckyNUmber[i]);
+    }
+    
+    return 0;
+}
